f2ed68e5c809b131d0489faf6d42c3f73076029c
Micheal Oshogwe <kenneth.oshogwe@gmail.com>
2024-05-31 17:21:08 +0100

    finished the getstarted component

--- src/components/getStarted/GetStarted.jsx
+++ src/components/getStarted/GetStarted.jsx
@@ -2,7 +2,24 @@ import React from "react";
 import "./GetStarted.css";
 
 const GetStarted = () => {
-  return <div>GetStarted</div>;
+  return (
+    <section className="g-wrapper">
+      <div className="paddings innerWidth g-container">
+        <div className="flexColCenter inner-container">
+          <span className="primaryText">Get started with Homyzy</span>
+          <span className="secondaryText">
+            Subscribe and find super attractive price quotes from us all over
+            the world
+            <br />
+            Find your residence Soon
+          </span>
+          <button className="button">
+            <a href="mailto:kenneth.oshogwe@gmail.com">Get Started</a>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GetStarted;
